b08882d19e58558bfa38e978703fdc5908d5acf0
Michele Azzolari <michele@fluidware.it>
2023-10-31 06:42:23 +0100

    First release

---
+++ src/config.ts
@@ -0,0 +1,63 @@
+/*
+ * Copyright Fluidware srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { EnvParse } from '@fluidware-it/saddlebag';
+import * as fs from 'fs';
+import { ConnectionOptions } from 'mysql2';
+
+export const USE_READ_COMMITTED_ISOLATION = EnvParse.envBool('FW_DB_USE_READ_COMMITTED_ISOLATION', false);
+
+function getDbPassword(DB_PASSWORD_FILE: string, DB_PASSWORD: string): string {
+  if (DB_PASSWORD_FILE) {
+    return fs.readFileSync(DB_PASSWORD_FILE, 'utf8');
+  }
+  return DB_PASSWORD;
+}
+
+const memoizedOptions: { [prefix: string]: ConnectionOptions } = {};
+
+export function getMysqlConnectionOptions(prefix = ''): ConnectionOptions {
+  if (!memoizedOptions[prefix]) {
+    const DB_PASSWORD = EnvParse.envString(`FW_${prefix}DB_PASSWORD`, '');
+    const DB_PASSWORD_FILE = EnvParse.envString(`FW_${prefix}DB_PASSWORD_FILE`, '');
+
+    if (!DB_PASSWORD && !DB_PASSWORD_FILE) {
+      throw new Error('FW_DB_PASSWORD or FW_DB_PASSWORD_FILE env is required');
+    }
+
+    const DB_USER = EnvParse.envStringRequired(`FW_${prefix}DB_USER`);
+    const DB_HOST = EnvParse.envString(`FW_${prefix}DB_HOST`, 'localhost');
+    const DB_PORT = EnvParse.envInt(`FW_${prefix}DB_PORT`, 3306);
+    const DB_NAME = EnvParse.envString(`FW_${prefix}DB_NAME`, DB_USER);
+    const DB_CONN_OPTIONS = EnvParse.envJSON(`FW_${prefix}DB_NAME`, {});
+
+    const dbPassword = getDbPassword(DB_PASSWORD_FILE, DB_PASSWORD);
+    const dbOptions = {
+      host: DB_HOST,
+      port: DB_PORT,
+      user: DB_USER,
+      password: dbPassword,
+      database: DB_NAME
+    };
+    if (Object.keys(DB_CONN_OPTIONS).length > 0) {
+      // See https://github.com/mysqljs/mysql#connection-options for all possible options
+      const customOptions = JSON.parse(DB_CONN_OPTIONS);
+      Object.assign(dbOptions, customOptions);
+    }
+    memoizedOptions[prefix] = dbOptions;
+  }
+  return memoizedOptions[prefix];
+}
---
+++ src/dbClient.ts
@@ -0,0 +1,154 @@
+/*
+ * Copyright Fluidware srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  createConnection,
+  Connection,
+  RowDataPacket,
+  ResultSetHeader,
+  ProcedureCallPacket,
+  FieldPacket
+} from 'mysql2/promise';
+import { getMysqlConnectionOptions, USE_READ_COMMITTED_ISOLATION } from './config';
+import { ConnectionOptions } from 'mysql2';
+
+interface ConnectionWrap extends Connection {
+  run<T extends ResultSetHeader | ResultSetHeader[] | RowDataPacket[] | RowDataPacket[][] | ProcedureCallPacket>(
+    sql: string,
+    phs?: (string | number | boolean)[]
+  ): Promise<{
+    rows: T;
+    cols: FieldPacket[];
+  }>;
+}
+
+export class DbClient {
+  private connection?: ConnectionWrap;
+  private connectionOptions: ConnectionOptions;
+
+  constructor(connectionOptionsOrPrefix?: ConnectionOptions | string) {
+    if (connectionOptionsOrPrefix) {
+      if (typeof connectionOptionsOrPrefix === 'string') {
+        this.connectionOptions = getMysqlConnectionOptions(connectionOptionsOrPrefix);
+      } else {
+        this.connectionOptions = connectionOptionsOrPrefix;
+      }
+    } else {
+      this.connectionOptions = getMysqlConnectionOptions('');
+    }
+  }
+
+  async open() {
+    this.connection = (await createConnection(this.connectionOptions)) as ConnectionWrap;
+    this.connection.run = async <
+      T extends ResultSetHeader | ResultSetHeader[] | RowDataPacket[] | RowDataPacket[][] | ProcedureCallPacket
+    >(
+      sql: string,
+      phs?: (string | number | boolean)[]
+    ): Promise<{ rows: T; cols: FieldPacket[] }> => {
+      if (!this.connection) {
+        throw new Error('run() called but no connection available');
+      }
+      const [rows, cols] = await this.connection.execute<T>(sql, phs);
+      return {
+        rows,
+        cols
+      };
+    };
+  }
+
+  getConnection(): ConnectionWrap | undefined {
+    return this.connection;
+  }
+
+  async close() {
+    if (this.connection) {
+      await this.connection.end();
+    }
+  }
+
+  async startTransaction() {
+    if (!this.connection) {
+      throw new Error('no connection available');
+    }
+    if (USE_READ_COMMITTED_ISOLATION) {
+      await this.connection.execute("SET SESSION tx_isolation='read-committed'");
+    }
+    await this.connection.execute('SET AUTOCOMMIT=0');
+  }
+
+  async commit(closeTransaction = true) {
+    if (!this.connection) {
+      throw new Error('no connection available');
+    }
+    await this.connection.execute('COMMIT');
+    if (closeTransaction) {
+      await this.connection.execute('SET AUTOCOMMIT=1');
+    }
+  }
+
+  async rollback(closeTransaction = true) {
+    if (!this.connection) {
+      throw new Error('no connection available');
+    }
+
+    await this.connection.execute('ROLLBACK');
+    if (closeTransaction) {
+      await this.connection.execute('SET AUTOCOMMIT=1');
+    }
+  }
+
+  async all(sql: string, phs?: (string | number | boolean)[]) {
+    if (!this.connection) {
+      throw new Error('no connection available');
+    }
+    const res = await this.connection.run<RowDataPacket[]>(sql, phs);
+    return res.rows;
+  }
+
+  async get(sql: string, phs?: (string | number | boolean)[]) {
+    if (!this.connection) {
+      throw new Error('no connection available');
+    }
+    const res = await this.connection.run<RowDataPacket[]>(sql, phs);
+    if (res.rows.length > 1) throw new Error('get returned more than one row');
+    return res.rows[0];
+  }
+
+  async insert(sql: string, phs?: (string | number | boolean)[]) {
+    if (!this.connection) {
+      throw new Error('no connection available');
+    }
+    const res = await this.connection.run<ResultSetHeader>(sql, phs);
+    return res.rows.insertId || res.rows.affectedRows;
+  }
+
+  async update(sql: string, phs?: (string | number | boolean)[]) {
+    if (!this.connection) {
+      throw new Error('no connection available');
+    }
+    const res = await this.connection.run<ResultSetHeader>(sql, phs);
+    return res.rows.affectedRows;
+  }
+
+  async delete(sql: string, phs?: (string | number | boolean)[]) {
+    if (!this.connection) {
+      throw new Error('no connection available');
+    }
+    const res = await this.connection.run<ResultSetHeader>(sql, phs);
+    return res.rows.affectedRows;
+  }
+}
